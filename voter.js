6f7525a95f5229b7c74d59d4cf90de6b0e7d6943
orrburgel <41600149+orrburgel@users.noreply.github.com>
2020-12-01 17:11:55 +0200

    Create voter.js

---
+++ voter.js
@@ -0,0 +1,7 @@
+console.log("testing script updates remotely");
+
+//176971
+//176970
+//176973
+//document.getElementsByTagName("form")[0].submit();
+//`https://steamcommunity.com/openid/login?openid.ns=http://specs.openid.net/auth/2.0&openid.mode=checkid_setup&openid.return_to=https://ark-servers.net/server/${server}/vote/action/&openid.realm=https://ark-servers.net&openid.ns.sreg=http://openid.net/extensions/sreg/1.1&openid.claimed_id=http://specs.openid.net/auth/2.0/identifier_select&openid.identity=http://specs.openid.net/auth/2.0/identifier_select`
